b5c32bd787f4289e9253cbcfec7a91d30217f0a1
Nguyễn Anh Thư <annthu26112004@gmail.com>
2026-03-30 22:20:11 +0700

    Delete Module 5/MusicApp directory

--- Module 5/MusicApp/src/main/java/com/example/musicapp/repository/ISongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.musicapp.repository;
-
-import com.example.musicapp.entity.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ISongRepository extends JpaRepository<Song, Integer> {
-}
